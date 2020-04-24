9cba3827668072d347df6e07bf4408cf1ecb2fb4
fanzheming <506954808@qq.com>
2020-04-24 23:25:19 +0800

    feat: frontend knowledage picture

--- docs/.vuepress/config/nav/index.js
+++ docs/.vuepress/config/nav/index.js
@@ -1,7 +1,7 @@
 module.exports = [
     { text: '主页', link: '/', icon: 'reco-home' },
     { text: '时间线', link: '/timeline/', icon: 'reco-date' },
-    { text: '知识体系', link: '/note/知识体系汇总', icon: 'reco-document' },
+    { text: '前端知识体系', link: '/note/前端知识体系', icon: 'reco-document' },
     {
         text: '联系我',
         icon: 'reco-message',
